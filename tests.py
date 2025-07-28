0af6e0e039cdc1c60d90774d484a8f5e5645a2fe
Yuseonr <rafianandra@gmail.com>
2025-07-28 21:59:17 +0700

    Add: function: write_file

--- tests.py
+++
@@ -1,7 +0,0 @@
-from functions.get_files_info import get_files_info
-
-try_dir = [('calculator','.'), ("calculator", "pkg"),("calculator", "/bin"),("calculator", "../")]
-
-for work_dir, directory in try_dir :
-    print(f"Result for '{directory}' directory:")
-    print(get_files_info(work_dir,directory))
